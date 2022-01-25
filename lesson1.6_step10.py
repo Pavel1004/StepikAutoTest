defc891217d19e3a67b333bf01fc4b089923ffe4
Pavel1004 <pavel.verbin@qoden.com>
2022-01-25 17:04:43 +0300

    lesson1.6_step10

---
+++ lesson1.6_step10.py
@@ -0,0 +1,34 @@
+from selenium import webdriver
+import time
+
+try:
+    link = "http://suninjuly.github.io/registration1.html"
+    browser = webdriver.Chrome()
+    browser.get(link)
+
+    # Ваш код, который заполняет обязательные поля
+    firstname = browser.find_element_by_css_selector('[placeholder="Input your first name"]')
+    firstname.send_keys("Pavel")
+    lastname = browser.find_element_by_css_selector('[placeholder="Input your last name"]')
+    lastname.send_keys("Pavlov")
+    email = browser.find_element_by_css_selector('[placeholder="Input your email"]')
+    email.send_keys("pavlov@mail.ru")
+    # Отправляем заполненную форму
+    button = browser.find_element_by_css_selector("button.btn")
+    button.click()
+
+    # Проверяем, что смогли зарегистрироваться
+    # ждем загрузки страницы
+    time.sleep(1)
+
+    # находим элемент, содержащий текст
+    welcome_text_elt = browser.find_element_by_tag_name("h1")
+    # записываем в переменную welcome_text текст из элемента welcome_text_elt
+    welcome_text = welcome_text_elt.text
+
+    # с помощью assert проверяем, что ожидаемый текст совпадает с текстом на странице сайта
+    assert "Congratulations! You have successfully registered!" == welcome_text
+
+finally:
+    time.sleep(5)
+    browser.quit()
